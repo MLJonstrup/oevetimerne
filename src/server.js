84483241b5909b57dc43bbc3e08adaef608af5e0
Michael Lindholm Jonstrup <mijo22ab@student.cbs.dk>
2023-09-22 14:07:58 +0200

    hsdg

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.get('/', (req, res) => {
-  res.redirect('/home.html');
+  res.redirect('client/home.html');
 });
 
 app.listen(3000, () => {
