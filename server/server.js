bdc668aba4fba74e1f9e4723d7840238aa0331c5
kkamph26 <131863220+kkamph26@users.noreply.github.com>
2023-12-11 16:34:17 +0100

    bb
    bbb

--- server/server.js
+++ server/server.js
@@ -1,48 +1,3 @@
-// const https = require("https");
-// const fs = require("fs");
-// const express = require("express");
-// const cors = require("cors");
-// const path = require("path");
-// const responseTime = require('response-time');
-
-// const app = express();
-
-// const postRoute = require("./routes/post.js");
-// const commentRoute = require("./routes/comment.js");
-
-// const options = {
-//   key: fs.readFileSync("privkey.pem"), //HTTPS private key
-//   cert: fs.readFileSync("newcert.pem"), //HTTPS certificate
-// };
-
-// // Middlewares
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.static(path.join(__dirname, "../client")));
-
-// // Sending client files from server
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/pages/home.html"));
-// });
-
-// app.get("/chat", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/pages/chat.html"));
-// });
-
-// app.get("/store", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/pages/store.html"));
-// });
-
-// app.use("/post", postRoute);
-// app.use("/comments", commentRoute);
-
-// // app.use("/customer", customerRoute);
-
-// // Start server
-// https.createServer(options, app).listen(3001, () => {
-//   console.log("HTTPS server listening on port 3001"); //HTTPS
-// });
-
 const https = require("https");
 const httpProxy = require("http-proxy");
 const express = require("express");
@@ -147,3 +102,48 @@ app.use("/comments", commentRoute);
 https.createServer(sslOptions, app).listen(3002, () => {
   console.log("HTTPS server listening on port 3002");
 });
+
+// const https = require("https");
+// const fs = require("fs");
+// const express = require("express");
+// const cors = require("cors");
+// const path = require("path");
+// const responseTime = require('response-time');
+
+// const app = express();
+
+// const postRoute = require("./routes/post.js");
+// const commentRoute = require("./routes/comment.js");
+
+// const options = {
+//   key: fs.readFileSync("privkey.pem"), //HTTPS private key
+//   cert: fs.readFileSync("newcert.pem"), //HTTPS certificate
+// };
+
+// // Middlewares
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, "../client")));
+
+// // Sending client files from server
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/pages/home.html"));
+// });
+
+// app.get("/chat", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/pages/chat.html"));
+// });
+
+// app.get("/store", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/pages/store.html"));
+// });
+
+// app.use("/post", postRoute);
+// app.use("/comments", commentRoute);
+
+// // app.use("/customer", customerRoute);
+
+// // Start server
+// https.createServer(options, app).listen(3001, () => {
+//   console.log("HTTPS server listening on port 3001"); //HTTPS
+// });
