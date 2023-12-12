58210f5a8d45a4e02622f7524aedaaa6b62539dc
Michael Lindholm Jonstrup <mijo22ab@student.cbs.dk>
2023-12-12 12:27:13 +0100

    asd

--- client/scripts/forum.js
+++ client/scripts/forum.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const userId = 12; // Replace with logic to get user ID from a cookie
+  const userId = 12; //vores user id skal komeme fra en cookie
 
   async function fetchUserPosts() {
     try {
@@ -11,7 +11,31 @@ document.addEventListener("DOMContentLoaded", () => {
       const postsContainer = document.getElementById("topics");
 
       posts.forEach((post) => {
-        const postDiv = createPostDiv(post);
+        const postDiv = document.createElement("div");
+        postDiv.id = `post_${post.id}`;
+        postDiv.classList.add("post");
+
+        const titleDiv = document.createElement("div");
+        titleDiv.textContent = post.title;
+        postDiv.appendChild(titleDiv);
+
+        const dateDiv = document.createElement("div");
+        dateDiv.textContent = post.postDate;
+        postDiv.appendChild(dateDiv);
+
+        const contentDiv = document.createElement("div");
+        contentDiv.textContent = post.content;
+        postDiv.appendChild(contentDiv);
+
+        const imgDiv = document.createElement("div");
+        imgDiv.classList.add("img");
+        imgDiv.style.backgroundImage = `url(${post.imgUrl})`;
+        postDiv.appendChild(imgDiv);
+
+        postDiv.addEventListener("click", () =>
+          fetchAndDisplayComments(post.id)
+        );
+
         postsContainer.appendChild(postDiv);
       });
     } catch (error) {
@@ -22,135 +46,97 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function fetchAndDisplayComments(postId) {
     try {
-      const response = await fetch(`/comments/post/${postId}`);
-      if (!response.ok)
-        throw new Error(`HTTP error! status: ${response.status}`);
+        const response = await fetch(`/comments/post/${postId}`);
+        if (!response.ok)
+            throw new Error(`HTTP error! status: ${response.status}`);
 
-      const comments = await response.json();
-      const threadContainer = document.getElementById("thread");
+        const comments = await response.json();
 
-      clearThreadContainer(threadContainer);
+        const threadContainer = document.getElementById("thread");
 
-      // Create a textarea and button for adding comments
-      const commentText = createCommentTextarea();
-      const addCommentBtn = createAddCommentButton();
+        // Clear the existing content in the thread container
+        threadContainer.innerHTML = "";
 
-      threadContainer.appendChild(commentText);
-      threadContainer.appendChild(addCommentBtn);
+        // Create a textarea for entering comments
+        const commentText = document.createElement("textarea");
+        commentText.id = "commentText";
+        commentText.placeholder = "Enter your comment";
+        threadContainer.appendChild(commentText);
 
-      // Event listener for adding comments
-      addCommentBtn.addEventListener("click", async () => {
-        const commentContent = commentText.value.trim();
+        // Create a button for adding comments
+        const addCommentBtn = document.createElement("button");
+        addCommentBtn.id = "addCommentBtn";
+        addCommentBtn.textContent = "Add Comment";
+        threadContainer.appendChild(addCommentBtn);
 
-        // Check if commentContent is not empty
-        if (commentContent === "") {
-          alert("Please enter a comment.");
-          return;
-        }
+        // Event listener for adding comments
+        addCommentBtn.addEventListener("click", async () => {
+            const commentContent = commentText.value.trim();
 
-        const newComment = {
-          postId: postId,
-          commentsContent: commentContent,
-          commentsAuthor: userId,
-        };
+            // Check if commentContent is not empty
+            if (commentContent === "") {
+                alert("Please enter a comment.");
+                return;
+            }
 
-        try {
-          const response = await fetch("/comments/createComment", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newComment),
-          });
+            const newComment = {
+                postId: postId,
+                commentsContent: commentContent,
+                commentsAuthor: userId,
+            };
 
-          if (response.ok) {
-            // Append the new comment to the thread
-            appendComment(newComment);
+            try {
+                const response = await fetch("/comments/createComment", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(newComment),
+                });
 
-            // Clear the comment textarea
-            commentText.value = "";
-          } else {
-            alert("Error adding comment. Please try again.");
-            console.error("Error adding comment:", response.status);
-          }
-        } catch (error) {
-          alert("Error adding comment. Please try again.");
-          console.error("An error occurred:", error);
-        }
-      });
+                if (response.ok) {
+                    // Append the new comment to the thread
+                    appendComment(newComment);
 
-      // Append existing comments to the thread container
-      comments.forEach((comment) => {
-        appendComment(comment);
-      });
+                    // Clear the comment textarea
+                    commentText.value = "";
+                } else {
+                    alert("Error adding comment. Please try again.");
+                    console.error("Error adding comment:", response.status);
+                }
+            } catch (error) {
+                alert("Error adding comment. Please try again.");
+                console.error("An error occurred:", error);
+            }
+        });
+
+        comments.forEach((comment) => {
+            // Append comments to the thread container
+            appendComment(comment);
+        });
     } catch (error) {
-      alert("Failed to fetch comments.");
-      console.error("An error occurred:", error);
+        alert("Failed to fetch comments.");
+        console.error("An error occurred:", error);
     }
-  }
 
-  function createPostDiv(post) {
-    const postDiv = document.createElement("div");
-    postDiv.id = `post_${post.id}`;
-    postDiv.classList.add("post");
+    // Define the appendComment function
+    function appendComment(comment) {
+        const commentDiv = document.createElement("div");
+        const threadContainer = document.getElementById("thread");
+        commentDiv.classList.add("comment");
 
-    const titleDiv = document.createElement("div");
-    titleDiv.textContent = post.title;
-    postDiv.appendChild(titleDiv);
+        const authorDiv = document.createElement("div");
+        authorDiv.textContent = `Author: ${comment.commentsAuthor}`;
 
-    const dateDiv = document.createElement("div");
-    dateDiv.textContent = post.postDate;
-    postDiv.appendChild(dateDiv);
+        const contentDiv = document.createElement("div");
+        contentDiv.textContent = comment.commentsContent;
 
-    const contentDiv = document.createElement("div");
-    contentDiv.textContent = post.content;
-    postDiv.appendChild(contentDiv);
+        commentDiv.appendChild(authorDiv);
+        commentDiv.appendChild(contentDiv);
 
-    const imgDiv = document.createElement("div");
-    imgDiv.classList.add("img");
-    imgDiv.style.backgroundImage = `url(${post.imgUrl})`;
-    postDiv.appendChild(imgDiv);
-
-    postDiv.addEventListener("click", () => fetchAndDisplayComments(post.id));
-
-    return postDiv;
-  }
-
-  function clearThreadContainer(container) {
-    // Clear the existing content in the thread container
-    container.innerHTML = "";
-  }
-
-  function createCommentTextarea() {
-    const commentText = document.createElement("textarea");
-    commentText.id = "commentText";
-    commentText.placeholder = "Enter your comment";
-    return commentText;
-  }
-
-  function createAddCommentButton() {
-    const addCommentBtn = document.createElement("button");
-    addCommentBtn.id = "addCommentBtn";
-    addCommentBtn.textContent = "Add Comment";
-    return addCommentBtn;
-  }
-
-  function appendComment(comment) {
-    const commentDiv = document.createElement("div");
-    const threadContainer = document.getElementById("thread");
-    commentDiv.classList.add("comment");
-
-    const authorDiv = document.createElement("div");
-    authorDiv.textContent = `Author: ${comment.commentsAuthor}`;
-
-    const contentDiv = document.createElement("div");
-    contentDiv.textContent = comment.commentsContent;
-
-    commentDiv.appendChild(authorDiv);
-    commentDiv.appendChild(contentDiv);
-
-    threadContainer.appendChild(commentDiv);
-  }
+        threadContainer.appendChild(commentDiv);
+    }
+}
 
   fetchUserPosts();
 });
--- server/routes/post.js
+++ server/routes/post.js
@@ -25,99 +25,24 @@ router.get("/updatePost", (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/pages/updatePost.html"));
 });
 
-// Create post route
-router.post("/createPost", async (req, res) => {
-  const { title, productId, postAuthor, content, stars, imgUrl } = req.body;
+// New route to fetch all posts
+router.get("/posts", (req, res) => {
+  console.log("Received request for /posts");
+
   const query = `
-    INSERT INTO posts (title, productId, postAuthor, content, stars, imgUrl)
-    VALUES (?, ?, ?, ?, ?, ?);
-    `;
-  db.run(
-    query,
-    [title, productId, postAuthor, content, stars, imgUrl],
-    function (err) {
-      if (err) {
-        console.error(err.message);
-        res.status(500).json({ error: "Error creating post" });
-      } else {
-        console.log(`Post created successfully.`);
-        res.status(200).json({ message: "Post created successfully!" });
-      }
-    }
-  );
-});
-
-router.put("/updatePost", async (req, res) => {
-  const { userId, postId, imgUrl, stars, content, productId, title } = req.body;
-
-  // Query to update a post by ID and author
-  const query = `
-        UPDATE posts
-        SET
-        title = COALESCE(?, title),
-        productId = COALESCE(?, productId),
-        content = COALESCE(?, content),
-        stars = COALESCE(?, stars),
-        imgUrl = COALESCE(?, imgUrl)
-        WHERE
-        id = ? AND postAuthor = ?;
-    `;
-
-  // Run the query
-  db.run(
-    query,
-    [
-      title || null,
-      productId || null,
-      content || null,
-      stars || null,
-      imgUrl || null,
-      postId,
-      userId,
-    ],
-    function (err) {
-      if (err) {
-        console.error(err.message);
-        res.status(500).json({
-          error: "Post update failed. Make sure you're the author of the post.",
-        });
-      } else if (this.changes === 0) {
-        console.error(err.message);
-        res.status(500).json({
-          error: "Post not found or you don't have permission to update it.",
-        });
-      } else {
-        console.log(`Post updated successfully.`);
-        res.status(200).json({ message: "Post updated successfully!" });
-      }
-    }
-  );
-});
-
-router.delete("/deletePost", async (req, res) => {
-  const { postId, userId } = req.body;
-  //const {userId} = req.cookies.userId; FOR LATER WHEN USER COOKIE IS IMPLEMENTED
-
-  //query to run
-  const query = `
-    DELETE FROM posts
-    WHERE id = ? AND postAuthor = ?;
+    SELECT *
+    FROM posts
+    ORDER BY postDate DESC
+    LIMIT 20;
   `;
-  //running the sqlite3 query
-  db.run(query, [postId, userId], function (err) {
+  // Run the query
+  db.all(query, (err, rows) => {
     if (err) {
       console.error(err.message);
-      res.status(500).json({
-        error: "Post deletion failed. Make sure you're the author of the post.",
-      });
-    } else if (this.changes === 0) {
-      console.error(err.message);
-      res.status(500).json({
-        error: "Post not found or you don't have permission to delete it.",
-      });
+      res.status(500).json({ error: "Error fetching posts" });
     } else {
-      console.log(`Post deleted successfully.`);
-      res.status(200).json({ message: "Post deleted successfully!" });
+      console.log(`Last 20 posts fetched successfully.`);
+      res.status(200).json(rows);
     }
   });
 });
@@ -126,9 +51,9 @@ router.get("/userPosts/:userId", (req, res) => {
   const userId = req.params.userId;
   // Query to fetch user posts
   const query = `
-      SELECT * FROM posts
-      WHERE postAuthor = ?;
-    `;
+    SELECT * FROM posts
+    WHERE postAuthor = ?;
+  `;
 
   // Run the query
   db.all(query, [userId], (err, rows) => {
@@ -142,26 +67,5 @@ router.get("/userPosts/:userId", (req, res) => {
   });
 });
 
-router.get("/posts", (req, res) => {
-  console.log("Received request for /post/posts");
-
-  const query = `
-    SELECT *
-    FROM posts
-    ORDER BY postDate DESC
-    LIMIT 20;
-  `;
-  // Run the query
-  db.all(query, (err, rows) => {
-    if (err) {
-      console.error(err.message);
-      res.status(500).json({ error: "Error fetching user posts" });
-    } else {
-      console.log(`Last 20 posts fetched successfully.`);
-      res.status(200).json(rows);
-    }
-  });
-});
-
-
 module.exports = router;
+ 
