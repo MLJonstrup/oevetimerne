d2ac0d7eabf59ac882ef9d73a891db23ff3b4b2e
Michael Lindholm Jonstrup <mijo22ab@student.cbs.dk>
2023-12-12 12:10:01 +0100

    asddsa

--- client/scripts/forum.js
+++ client/scripts/forum.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const userId = 12; //vores user id skal komeme fra en cookie
+  const userId = 12; // Replace with logic to get user ID from a cookie
 
   async function fetchUserPosts() {
     try {
@@ -11,31 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const postsContainer = document.getElementById("topics");
 
       posts.forEach((post) => {
-        const postDiv = document.createElement("div");
-        postDiv.id = `post_${post.id}`;
-        postDiv.classList.add("post");
-
-        const titleDiv = document.createElement("div");
-        titleDiv.textContent = post.title;
-        postDiv.appendChild(titleDiv);
-
-        const dateDiv = document.createElement("div");
-        dateDiv.textContent = post.postDate;
-        postDiv.appendChild(dateDiv);
-
-        const contentDiv = document.createElement("div");
-        contentDiv.textContent = post.content;
-        postDiv.appendChild(contentDiv);
-
-        const imgDiv = document.createElement("div");
-        imgDiv.classList.add("img");
-        imgDiv.style.backgroundImage = `url(${post.imgUrl})`;
-        postDiv.appendChild(imgDiv);
-
-        postDiv.addEventListener("click", () =>
-          fetchAndDisplayComments(post.id)
-        );
-
+        const postDiv = createPostDiv(post);
         postsContainer.appendChild(postDiv);
       });
     } catch (error) {
@@ -46,97 +22,135 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function fetchAndDisplayComments(postId) {
     try {
-        const response = await fetch(`/comments/post/${postId}`);
-        if (!response.ok)
-            throw new Error(`HTTP error! status: ${response.status}`);
+      const response = await fetch(`/comments/post/${postId}`);
+      if (!response.ok)
+        throw new Error(`HTTP error! status: ${response.status}`);
 
-        const comments = await response.json();
+      const comments = await response.json();
+      const threadContainer = document.getElementById("thread");
 
-        const threadContainer = document.getElementById("thread");
+      clearThreadContainer(threadContainer);
 
-        // Clear the existing content in the thread container
-        threadContainer.innerHTML = "";
+      // Create a textarea and button for adding comments
+      const commentText = createCommentTextarea();
+      const addCommentBtn = createAddCommentButton();
 
-        // Create a textarea for entering comments
-        const commentText = document.createElement("textarea");
-        commentText.id = "commentText";
-        commentText.placeholder = "Enter your comment";
-        threadContainer.appendChild(commentText);
+      threadContainer.appendChild(commentText);
+      threadContainer.appendChild(addCommentBtn);
 
-        // Create a button for adding comments
-        const addCommentBtn = document.createElement("button");
-        addCommentBtn.id = "addCommentBtn";
-        addCommentBtn.textContent = "Add Comment";
-        threadContainer.appendChild(addCommentBtn);
+      // Event listener for adding comments
+      addCommentBtn.addEventListener("click", async () => {
+        const commentContent = commentText.value.trim();
 
-        // Event listener for adding comments
-        addCommentBtn.addEventListener("click", async () => {
-            const commentContent = commentText.value.trim();
+        // Check if commentContent is not empty
+        if (commentContent === "") {
+          alert("Please enter a comment.");
+          return;
+        }
 
-            // Check if commentContent is not empty
-            if (commentContent === "") {
-                alert("Please enter a comment.");
-                return;
-            }
+        const newComment = {
+          postId: postId,
+          commentsContent: commentContent,
+          commentsAuthor: userId,
+        };
 
-            const newComment = {
-                postId: postId,
-                commentsContent: commentContent,
-                commentsAuthor: userId,
-            };
+        try {
+          const response = await fetch("/comments/createComment", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newComment),
+          });
 
-            try {
-                const response = await fetch("/comments/createComment", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(newComment),
-                });
+          if (response.ok) {
+            // Append the new comment to the thread
+            appendComment(newComment);
 
-                if (response.ok) {
-                    // Append the new comment to the thread
-                    appendComment(newComment);
+            // Clear the comment textarea
+            commentText.value = "";
+          } else {
+            alert("Error adding comment. Please try again.");
+            console.error("Error adding comment:", response.status);
+          }
+        } catch (error) {
+          alert("Error adding comment. Please try again.");
+          console.error("An error occurred:", error);
+        }
+      });
 
-                    // Clear the comment textarea
-                    commentText.value = "";
-                } else {
-                    alert("Error adding comment. Please try again.");
-                    console.error("Error adding comment:", response.status);
-                }
-            } catch (error) {
-                alert("Error adding comment. Please try again.");
-                console.error("An error occurred:", error);
-            }
-        });
-
-        comments.forEach((comment) => {
-            // Append comments to the thread container
-            appendComment(comment);
-        });
+      // Append existing comments to the thread container
+      comments.forEach((comment) => {
+        appendComment(comment);
+      });
     } catch (error) {
-        alert("Failed to fetch comments.");
-        console.error("An error occurred:", error);
+      alert("Failed to fetch comments.");
+      console.error("An error occurred:", error);
     }
+  }
 
-    // Define the appendComment function
-    function appendComment(comment) {
-        const commentDiv = document.createElement("div");
-        const threadContainer = document.getElementById("thread");
-        commentDiv.classList.add("comment");
+  function createPostDiv(post) {
+    const postDiv = document.createElement("div");
+    postDiv.id = `post_${post.id}`;
+    postDiv.classList.add("post");
 
-        const authorDiv = document.createElement("div");
-        authorDiv.textContent = `Author: ${comment.commentsAuthor}`;
+    const titleDiv = document.createElement("div");
+    titleDiv.textContent = post.title;
+    postDiv.appendChild(titleDiv);
 
-        const contentDiv = document.createElement("div");
-        contentDiv.textContent = comment.commentsContent;
+    const dateDiv = document.createElement("div");
+    dateDiv.textContent = post.postDate;
+    postDiv.appendChild(dateDiv);
 
-        commentDiv.appendChild(authorDiv);
-        commentDiv.appendChild(contentDiv);
+    const contentDiv = document.createElement("div");
+    contentDiv.textContent = post.content;
+    postDiv.appendChild(contentDiv);
 
-        threadContainer.appendChild(commentDiv);
-    }
-}
+    const imgDiv = document.createElement("div");
+    imgDiv.classList.add("img");
+    imgDiv.style.backgroundImage = `url(${post.imgUrl})`;
+    postDiv.appendChild(imgDiv);
+
+    postDiv.addEventListener("click", () => fetchAndDisplayComments(post.id));
+
+    return postDiv;
+  }
+
+  function clearThreadContainer(container) {
+    // Clear the existing content in the thread container
+    container.innerHTML = "";
+  }
+
+  function createCommentTextarea() {
+    const commentText = document.createElement("textarea");
+    commentText.id = "commentText";
+    commentText.placeholder = "Enter your comment";
+    return commentText;
+  }
+
+  function createAddCommentButton() {
+    const addCommentBtn = document.createElement("button");
+    addCommentBtn.id = "addCommentBtn";
+    addCommentBtn.textContent = "Add Comment";
+    return addCommentBtn;
+  }
+
+  function appendComment(comment) {
+    const commentDiv = document.createElement("div");
+    const threadContainer = document.getElementById("thread");
+    commentDiv.classList.add("comment");
+
+    const authorDiv = document.createElement("div");
+    authorDiv.textContent = `Author: ${comment.commentsAuthor}`;
+
+    const contentDiv = document.createElement("div");
+    contentDiv.textContent = comment.commentsContent;
+
+    commentDiv.appendChild(authorDiv);
+    commentDiv.appendChild(contentDiv);
+
+    threadContainer.appendChild(commentDiv);
+  }
 
   fetchUserPosts();
 });
