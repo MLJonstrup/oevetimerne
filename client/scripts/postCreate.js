fb0c0ce086f63b60f3c9c0ad8f24f05dc854ca39
Shadi Mahmoud <shad@itu.dk>
2023-12-17 23:24:56 +0100

    qdw

--- client/scripts/postCreate.js
+++ client/scripts/postCreate.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
       if (response.ok) {
         setTimeout(() => {
-          window.location.href = "http://161.35.86.140/post";
+          window.location.href = "https://joejuiceforum.social/post";
         }, 3000);
         alert(
           "Post created successfully! You will be automatically redirected in a few seconds."
