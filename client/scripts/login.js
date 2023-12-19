dcdd99e4d3ed835f4a01f390e13b81017208761b
Shadi Mahmoud <shad@itu.dk>
2023-12-19 22:31:15 +0100

    nj

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,46 +1,42 @@
 async function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
-  loginInfo = {username , password};
+  const loginInfo = { username, password };
 
   try {
     const response = await fetch('/user/login', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: { 'Content-Type': 'application/json' },
       credentials: 'include',
       body: JSON.stringify(loginInfo),
     });
 
     if (response.ok) {
       console.log('User logged in successfully!');
-      await checkCookie();
+      window.location.href = '/post'; // Omdiriger til Joe's Forum
     } else {
       console.error('Wrong username/password:', response.status);
+      // Vis eventuelt en fejlmeddelelse her
     }
   } catch (error) {
     console.error('An error occurred:', error);
+    // Vis eventuelt en fejlmeddelelse her
   }
-};
+}
 
-async function checkCookie() {
+async function checkLoginStatusAndUpdateUI() {
   try {
-    const response = await fetch('/user/details', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',  // Add this line to include cookies
-    });
+    const response = await fetch('/check-login', { credentials: 'include' });
+    const data = await response.json();
 
-    if (response.ok) {
-      const userDetails = await response.json();
-      console.log('User ID:', userDetails.userId);
-    } else {
-      console.error('Error fetching user details:', response.status);
-    }
+    document.querySelectorAll('.loggedInLink').forEach(el => {
+      el.style.display = data.loggedIn ? 'block' : 'none';
+    });
   } catch (error) {
     console.error('An error occurred:', error);
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  checkLoginStatusAndUpdateUI();
+});
